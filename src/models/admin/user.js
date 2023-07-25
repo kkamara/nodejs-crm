9d1583a77e15e3e94cebf95adae164bb393756f2
kkamara <kamaracomputers@gmail.com>
2023-07-25 12:25:20 +0100

    Add admin/user.getStats()

---
+++ src/models/admin/user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { QueryTypes, } = require('sequelize');
+const config = require('../config');
+const db = require('../database');
+
+/**
+ * @param {Number} id The authenticated dashboard user's id.
+ * @return {object|false}
+ */
+const getStats = async (id) => {
+  let res = false;
+  try {
+    const [results, metadata] = await db.query(
+      `SELECT count(users.uid) as users_count
+      FROM users;`,
+    );
+    res = { users_count: results.users_count, }
+    return { ...res, }
+  } catch(err) {
+    return res;
+  }
+};
+
+module.exports = {
+  getStats,
+};
